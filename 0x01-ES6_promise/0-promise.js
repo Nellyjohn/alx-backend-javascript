f856700f336911f02be10784202731ef8818bf0a
Vivian <okpala_vivian@yahoo.com>
2024-06-27 09:34:36 +0000

    quick fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise(() => {});
 }
